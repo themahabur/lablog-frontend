315be657dbb3803992ea616f04f51157df117bdc
themahabur <mahabursoikot04@gmail.com>
2026-05-12 21:30:29 +0600

    Implement environment variable management with @t3-oss/env-nextjs and refactor API URLs in auth-client and user service

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_API: z.url(),
+    FRONTEND_API: z.url(),
+    API_URL: z.url(),
+    AUTH_URL: z.url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_BACKEND_API: z.url(),
+  },
+
+  runtimeEnv: {
+    BACKEND_API: process.env.BACKEND_API,
+    FRONTEND_API: process.env.FRONTEND_API,
+    API_URL: process.env.API_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
+  },
+});
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
+import { env } from "@/env"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5000"
+    baseURL: env.NEXT_PUBLIC_BACKEND_API
 })
